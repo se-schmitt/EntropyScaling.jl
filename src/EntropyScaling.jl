875ac84fefbe2087e13c416511537adc0e36d4f0
se-schmitt <se.s.schmitt@gmail.com>
2024-12-03 15:50:13 +0100

    Update main file

--- src/EntropyScaling.jl
+++ src/EntropyScaling.jl
@@ -2,8 +2,8 @@ module EntropyScaling
 
 # Load public modules
 using LsqFit
-using NLsolve
-using Optim
+using NonlinearSolve
+using Optimization
 using StatsBase
 using ForwardDiff
 
@@ -11,25 +11,22 @@ using ForwardDiff
 get_kBNAR() = (kB=1.380649e-23; NA=6.02214076e23; return (kB,NA,kB*NA))
 (kB, NA, R) = get_kBNAR()
 
-# Include files
-include("fit_entropy_scaling.jl")
-include("call_entropy_scaling.jl")
-include("correlation_fun.jl")
-include("zero_density_limit.jl")
+# General
+include("general/types.jl")
+include("general/scalings.jl")
+include("general/chapman_enskog.jl")
 
-# Include extensions
+# Utils
+include("utils/data.jl")
+include("utils/thermo.jl")
+
+# Models 
+include("models/framework.jl")
+
+# Extensions
 if !isdefined(Base,:get_extension)
     include("../ext/MicthermExt.jl")
     include("../ext/ClapeyronExt.jl")
 end
-
-# Export
-export fit_entropy_scaling, call_entropy_scaling
-
-# Export extension functions
-# MicthermExt
-export MicThermParam
-function MicThermParam end
-abstract type MicThermParamType end
 
 end
