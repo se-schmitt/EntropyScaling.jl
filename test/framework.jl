619815d27de66d58cca603a72929fe06183ff04c
se-schmitt <se.s.schmitt@gmail.com>
2024-12-15 20:02:27 +0100

    Fixed test

--- test/framework.jl
+++ test/framework.jl
@@ -79,7 +79,7 @@
 
             @test thermal_conductivity(model_1, 0.1e6, 294.7, [.5,.5]) ≈ 1.338512e-1 rtol=1e-5
             @test isapprox(self_diffusion_coefficient(model_2, 0.1e6, 298.15, [.25,.75]),[1.746913e-9,1.097029e-9]; rtol=1e-5)
-            @test MS_diffusion_coefficient(model_2, 0.1e6, 298.15, [.6,.4]) ≈ 3.333533e-9 rtol=1e-5
+            @test MS_diffusion_coefficient(model_2, 0.1e6, 298.15, [.6,.4]) ≈ 1.912959e-9 rtol=1e-5
             @test self_diffusion_coefficient(model_2, 0.1e6, 298.15, [0.,1.])[1] ≈ MS_diffusion_coefficient(model_2, 0.1e6, 298.15, [0.,1.])
         end
     end
