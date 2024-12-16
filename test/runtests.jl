caa29ed91abc99b3e07fddeb07e14d56ca2c9ac0
se-schmitt <se.s.schmitt@gmail.com>
2024-12-16 23:44:11 +0100

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 using EntropyScaling
 using Clapeyron
 using DelimitedFiles
-using EntropyScaling: BaseParam
+
 include("framework.jl")
 include("refprop_res.jl")
 include("chapman_enskog.jl")
