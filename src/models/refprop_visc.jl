f34fabe2e61095fb5a478882a94d1d2837303274
se-schmitt <se.s.schmitt@gmail.com>
2024-12-16 23:43:02 +0100

    revision refprop RES model

--- src/models/refprop_visc.jl
+++ src/models/refprop_visc.jl
@@ -5,17 +5,24 @@ export RefpropRESModel, RefpropRESParams
 """
 struct RefpropRESParams{P,T} <: AbstractEntropyScalingParams
     n::Matrix{T}
-    σ::Vector{T}
-    ε::Vector{T}
+    CE_model::ChapmanEnskogModel
     base::BaseParam{P}
 end
 
-get_m(m::RefpropRESParams) = FillArrays.Fill(1.0,length(m.ε))
+function RefpropRESParams(prop::AbstractTransportProperty, n::Matrix{T}, σ::Vector{T}, 
+                          ε::Vector{T}, Mw::Vector{T}) where T
+    
+    CE_model = ChapmanEnskogModel(repeat([""],length(Mw)),σ,ε,Mw,collision_integral=Neufeld())
+    base = BaseParam(prop, Mw)
+    return RefpropRESParams(n,CE_model,base)
+end
 
 """
     RefpropRESModel{T} <: AbstractEntropyScalingModel
 
-A generic entropy scaling model.
+Entropy scaling model based on Refprop EOS.
+
+## References
 """
 struct RefpropRESModel{E,FP} <: AbstractEntropyScalingModel
     components::Vector{String}
@@ -31,8 +38,6 @@ function scaling_model(param::RefpropRESParams{Viscosity}, s, x=z1)
 end
 
 function generic_powerseries_scaling_model(param, s, x, g)
-    ##α = a1,a2,a3,a4,ξ
-    #ln(η + 1) = a1*(s/ξ)^g1 + a2*(s/ξ)^g2 + a3*(s/ξ)^g3 + a4*(s/ξ)^g4
     n = param.n
     lnY⁺p1 = zero(Base.promote_eltype(n,s,x,g))
     @assert length(g) <= size(n,1)
@@ -49,25 +54,10 @@ end
 
 function scaling(param::RefpropRESParams, eos, Y, T, ϱ, s, z=Z1; inv=true)
     k = !inv ? 1 : -1
-    Y₀ = property_CE(param, T, z)
+    Y₀ = property_CE(transport_property(param), param.CE_model, T, z)
     if inv
         return plus_scaling(param.base, Y, T, ϱ, s, z; inv=true) + Y₀
     else
         return plus_scaling(param.base, Y - Y₀, ϱ, s, z; inv=false)
     end
-end
-
-viscosity_refprop(T,Mw,σ,ε) = viscosity_CE(T, Mw, σ, ε, Ω_22_neufeld(T*kB/ε))
-
-function property_CE(param::RefpropRESParams{Viscosity}, T, z = Z1; mixing = nothing)
-    Mw = param.base.Mw
-    σ,ε = param.σ,param.ε
-    prop = transport_property(param)
-    if length(Mw) == 1
-        Y₀ = viscosity_refprop(T,Mw[1],σ[1],ε[1])*one(eltype(z))
-    else
-        Y₀_all = viscosity_refprop.(T,Mw,σ,ε)
-        Y₀ = mix_CE(mixing, param.base, Y₀⁺_all)
-    end
-    return Y₀
 end
