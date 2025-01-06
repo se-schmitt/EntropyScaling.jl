7c4a32e8e97a8b1be96e5b442332373ba07093f2
se-schmitt <se.s.schmitt@gmail.com>
2025-01-06 15:04:45 +0100

    Add references through `DocumenterCitations.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,24 @@
-using Documenter, EntropyScaling
+using Documenter, DocumenterCitations, EntropyScaling
+
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:numeric)
 
 makedocs(
     sitename="EntropyScaling.jl",
     format = Documenter.HTML(
         canonical = "https://se-schmitt.github.io/EntropyScaling.jl/",
-        # assets = ["assets/logo.ico"],
+        assets = ["assets/citations.css"],
     ),
     pages = [
         "Home" => "index.md",
         "Getting Started" => "getting_started.md",
         "Transport Properties" => "transport_properties.md",
-        "Models" => "models.md",
-    ]
+        "Models" => [
+            "models/ES_models.md",
+            "models/CE_models.md"
+        ],
+        "References" => "references.md"
+    ],
+    plugins=[bib]
 )
 
 deploydocs(
