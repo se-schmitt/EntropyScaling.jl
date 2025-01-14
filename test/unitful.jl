46bdeb8feaa9fda10db1f1bc13dca8871899ed5c
se-schmitt <se.s.schmitt@gmail.com>
2025-01-14 23:58:37 +0100

    Shorten unitful tests

--- test/unitful.jl
+++ test/unitful.jl
@@ -1,144 +1,40 @@
-@testset "Unitful Extension" begin
+@testset "UnitfulExt" begin
 
-    import Unitful: K, °C, bar, atm, Pa, s, mol, m, percent, W
-    import Unitful: btu, hr, ft, Ra, cP, μPa, mW, psi, Ra
+    @testset "Data Constructors" begin
+        ηdatu = ViscosityData([300.0u"K"], [1u"bar"], [1u"mPa*s"])
+        ηdatU = TransportPropertyData([300.0u"K"], [1u"bar"], [1u"cP"])
+        ηdat = ViscosityData([300.], [1e5], nothing, [1e-3])
+        @test all([getfield(ηdatu,sym) == getfield(ηdatU,sym) == getfield(ηdat,sym) for sym in [:T,:p,:Y]])
 
-    Ts = collect(LinRange(10, 80, 15) * °C)
-    Ps = [1bar, 2bar, 5bar, 50bar, 1000.0bar]
+        λdatu = ThermalConductivityData([25u"°C"], [10u"mol/l"], [0.15u"W/(m*K)"])
+        λdatU = TransportPropertyData([25u"°C"], [10u"mol/l"], [0.15u"W/(m*K)"])
+        λdat = ThermalConductivityData([298.15], nothing, [10000.], [0.15])
+        @test all([getfield(λdatu,sym) == getfield(λdatU,sym) == getfield(λdat,sym) for sym in [:T,:ϱ,:Y]])
 
-    fluid = "Toluene"
+        Ddatu = SelfDiffusionCoefficientData([80.33u"°F"], [1u"bar"], [1e-5u"cm^2/s"])
+        Ddat = SelfDiffusionCoefficientData([300.], [1e5], nothing, [1e-9])
+        @test all([getfield(Ddatu,sym) == getfield(Ddat,sym) for sym in [:T,:p,:Y]])
 
-    ϱ = typeof(1.0 * mol / m^3)[]
-    λ = typeof(1.0 * W / m / K)[]
-    η = typeof(1.0 * Pa * s)[]
-
-    PT = []
-    P_data = typeof(1.0Pa)[]
-    T_data = typeof(1.0K)[]
-    for T in Ts
-        for p in Ps
-            push!(ϱ, PropsSI("DMOLAR", "P", p, "T", T, fluid))
-            push!(λ, PropsSI("L", "P", p, "T", T, fluid))
-            push!(η, PropsSI("VISCOSITY", "P", p, "T", T, fluid))
-            push!(PT, (p, T))
-            push!(P_data, p)
-            push!(T_data, T)
-        end
+        Dinfdatu = InfDiffusionCoefficientData([80.33u"°F"], [10u"mol/dm^3"], [1e-5u"cm^2/s"])
+        Dinfdat = InfDiffusionCoefficientData([300.], nothing, [10000.], [1e-9])
+        @test all([getfield(Dinfdatu,sym) == getfield(Dinfdat,sym) for sym in [:T,:ϱ,:Y]])
     end
 
+    @testset "Property functions" begin
 
-    data_eta_unit_density = ViscosityData(T_data, ϱ, η)
-    data_lamb_unit_density = ThermalConductivityData(T_data, ϱ, λ)
-    data_eta_unit_pressure = ViscosityData(T_data, P_data, η)
-    data_lamb_unit_pressure = ThermalConductivityData(T_data, P_data, λ)
+        model = FrameworkModel(PCSAFT("n-butane"),Dict(
+            Viscosity() => [[0.;-14.165;13.97;-2.382;0.501;;]],
+            ThermalConductivity() => [[3.962;98.222;-82.974;20.079;1.073;;]],
+            SelfDiffusionCoefficient() => [[0.;0.;0.;-3.507;-0.997;;]]
+        ))
 
-    @testset "Viscosity data generation" begin
+        @test viscosity(model, 37.21u"MPa", 323u"K"; output=u"cP").val ≈ 1.921922e-1 rtol=1e-5
+        @test thermal_conductivity(model, 372.1u"bar", 49.85u"°C").val ≈ 1.199070e-1 rtol=1e-5
+        @test self_diffusion_coefficient(model, 37.21u"MPa", 323u"K"; output=u"cm^2/s").val ≈ 6.645588e-5 rtol=1e-5
 
-
-        data_eta_ustrip_pressure = ViscosityData(
-            ustrip.(T_data .|> K),
-            ustrip.(P_data .|> Pa),
-            [],
-            ustrip.(η),
-            :unknown,
-        )
-        data_eta_ustrip_density =
-            ViscosityData(ustrip.(T_data .|> K), [], ustrip.(ϱ), ustrip.(η), :unknown)
-
-        @test all(data_eta_unit_pressure.T .== data_eta_ustrip_pressure.T)
-        @test all(data_eta_unit_pressure.p .== data_eta_ustrip_pressure.p)
-        @test all(data_eta_unit_pressure.Y .== data_eta_ustrip_pressure.Y)
-        @test all(data_eta_unit_density.T .== data_eta_ustrip_density.T)
-        @test all(data_eta_unit_density.Y .== data_eta_ustrip_density.Y)
-
-
-        data_lamb_ustrip_pressure = ThermalConductivityData(
-            ustrip.(T_data .|> K),
-            ustrip.(P_data .|> Pa),
-            [],
-            ustrip.(λ),
-            :unknown,
-        )
-        data_lamb_ustrip_density = ThermalConductivityData(
-            ustrip.(T_data .|> K),
-            [],
-            ustrip.(ϱ),
-            ustrip.(λ),
-            :unknown,
-        )
-
-        @test all(data_lamb_unit_pressure.T .== data_lamb_ustrip_pressure.T)
-        @test all(data_lamb_unit_pressure.p .== data_lamb_ustrip_pressure.p)
-        @test all(data_lamb_unit_pressure.Y .== data_lamb_ustrip_pressure.Y)
-        @test all(data_lamb_unit_density.T .== data_lamb_ustrip_density.T)
-        @test all(data_lamb_unit_density.Y .== data_lamb_ustrip_density.Y)
+        ϱmass = 602.436942723435u"kg/m^3"       #mass_density(model.eos, 37.21u"MPa", 323u"K")
+        ϱmol = 10365.398188634463u"mol/m^3"     #molar_density(model.eos, 37.21u"MPa", 323u"K")
+        @test viscosity(model, ϱmass, 323u"K").val ≈ 1.921922e-4 rtol=1e-5
+        @test thermal_conductivity(model, ϱmol, 49.85u"°C").val ≈ 1.199070e-1 rtol=1e-5
     end
-
-    ## Properties
-    eos_model = PCSAFT(fluid)
-
-    model_eta = FrameworkModel(eos_model, [data_eta_unit_density])
-    model_lamb = FrameworkModel(eos_model, [data_lamb_unit_density])
-
-    @testset "Viscosity" begin
-        for (i, (p, t)) ∈ enumerate(PT)
-            @test viscosity(model_eta, p, t) ≈ η[i] rtol = 1E-1
-        end
-    end
-
-    @testset "Thermal Conductivity" begin
-        for (i, (p, t)) ∈ enumerate(PT)
-            @test thermal_conductivity(model_lamb, p, t) ≈ λ[i] rtol = 1E-1
-        end
-    end
-
-    @testset "Unit Conversions" begin
-
-        T = 25.0°C
-        P = 1.0bar
-
-        η_calc = viscosity(model_eta, P, T, output_unit = cP)
-        λ_calc = thermal_conductivity(model_lamb, P, T, output_unit = btu / (hr * ft * Ra))
-
-        @test η_calc ≈ 552.57μPa * s rtol = 1E-1
-        @test λ_calc ≈ 130.35mW / m / K rtol = 1E-1
-
-        P_psi = P |> psi
-        T_Ra = T |> Ra
-
-        η_calc2 = viscosity(model_eta, P_psi, T_Ra)
-        λ_calc2 = thermal_conductivity(model_lamb, P_psi, T_Ra)
-
-        @test η_calc2 ≈ η_calc rtol = 1E-13
-        @test λ_calc2 ≈ λ_calc rtol = 1E-13
-    end
-
-
-    @testset "Transport Property Constructor" begin
-
-        data_transp_p_eta = EntropyScaling.TransportPropertyData(T_data, P_data, η)
-        data_transp_p_lamb = EntropyScaling.TransportPropertyData(T_data, P_data, λ)
-
-        data_transp_ϱ_eta = EntropyScaling.TransportPropertyData(T_data, ϱ, η)
-        data_transp_ϱ_lamb = EntropyScaling.TransportPropertyData(T_data, ϱ, λ)
-
-        @test data_transp_p_eta.prop isa EntropyScaling.Viscosity
-        @test data_transp_p_lamb.prop isa EntropyScaling.ThermalConductivity
-        @test data_transp_ϱ_eta.prop isa EntropyScaling.Viscosity
-        @test data_transp_ϱ_lamb.prop isa EntropyScaling.ThermalConductivity
-
-        @test all(data_transp_p_eta.T .== data_eta_unit_pressure.T)
-        @test all(data_transp_p_eta.p .== data_eta_unit_pressure.p)
-        @test all(data_transp_p_eta.Y .== data_eta_unit_pressure.Y)
-        @test all(data_transp_p_lamb.T .== data_lamb_unit_pressure.T)
-        @test all(data_transp_p_lamb.p .== data_lamb_unit_pressure.p)
-        @test all(data_transp_p_lamb.Y .== data_lamb_unit_pressure.Y)
-
-        @test all(data_transp_ϱ_eta.T .== data_eta_unit_density.T)
-        @test all(data_transp_ϱ_eta.Y .== data_eta_unit_density.Y)
-        @test all(data_transp_ϱ_lamb.T .== data_lamb_unit_density.T)
-        @test all(data_transp_ϱ_lamb.Y .== data_lamb_unit_density.Y)
-
-    end
-
 end
