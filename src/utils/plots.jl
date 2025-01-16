af428331fb71ee30ceab6769b3d1c1cca71eb2c7
se-schmitt <se.s.schmitt@gmail.com>
2025-01-16 11:43:57 +0100

    Add docstring for plot and small adjustments

--- src/utils/plots.jl
+++ src/utils/plots.jl
@@ -1,5 +1,19 @@
+"""
+    plot(model::AbstractEntropyScalingModel, dat::TransportPropertyData; slims=nothing, 
+         cprop=nothing)
+
+Plots the scaled transport property as function of the entropy scaling variable (e.g. the reduced entropy).
+The entropy scaling model as well as the scaled transport property data are shown.
+
+`slims` sets the range for the entropy scaling variable (used for the model calculation).
+`cprop` controls the property used for coloring (e.g. `:T`, `:p`, or `ϱ`).
+A general documentation for `plot` for controling the appearance can be found [here](https://docs.juliaplots.org/stable/).
+"""
+plot
+plot() = nothing
+
 @recipe function f(model::AbstractEntropyScalingModel, dat::TransportPropertyData; 
-                   slims=nothing, colprop=nothing )
+                   slims=nothing, cprop=nothing )
     # Preprocessing 
     param = model[dat.prop]
 
@@ -16,9 +30,9 @@
     Yˢx = scaling_model.(param, sˢx)
 
     # Settings
-    cb_title =  colprop == :T ? "T / K" : 
-                colprop == :p ? "p / Pa" : 
-                colprop == :ϱ ? "ϱ / mol/m³" : "$colprop"
+    cb_title =  cprop == :T ? "T / K" : 
+                cprop == :p ? "p / Pa" : 
+                cprop == :ϱ ? "ϱ / mol/m³" : "$cprop"
 
     # General 
     yscale --> :log10
@@ -26,7 +40,7 @@
     xlabel --> "sˢ"
     ylabel --> "$(symbol(dat.prop))ˢ"
     xlims --> slims
-    if !isnothing(colprop)
+    if !isnothing(cprop)
         colorbar_framestyle --> :box
         colorbar_title --> cb_title
     end
@@ -36,12 +50,13 @@
         seriestype := :scatter
         msw --> 0
         markersize --> 5
-        if isnothing(colprop)
+        if isnothing(cprop)
             markercolor --> :dodgerblue
         else
-            zcolor --> getfield(dat,colprop)
+            zcolor --> getfield(dat,cprop)
         end
-        label --> nothing
+        label := nothing
+        z_order --> 1
 
         sˢdat, Yˢdat
     end
