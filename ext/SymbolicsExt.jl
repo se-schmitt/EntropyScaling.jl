881a70a599082ee25b02366f9d0edfc7330fde3a
se-schmitt <se.s.schmitt@gmail.com>
2025-06-04 00:05:19 +0200

    avoid duplicate definition of promotion

--- ext/SymbolicsExt.jl
+++ ext/SymbolicsExt.jl
@@ -6,18 +6,18 @@ const ES = EntropyScaling
 
 @register_symbolic ES.ϱT_viscosity(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
 @register_symbolic ES.ϱT_thermal_conductivity(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
-@register_symbolic ES.ϱT_thermal_conductivity(model::ES.RefpropRESModel, ϱ, T, z::AbstractVector)
+@register_symbolic ES.ϱT_thermal_conductivity(model::ES.RefpropRESModel, ϱ, T, z::AbstractVector) false
 @register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
-@register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T)
-@register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.FrameworkModel, ϱ, T, z::AbstractVector)
+@register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T) false
+@register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.FrameworkModel, ϱ, T, z::AbstractVector) false
 @register_symbolic ES.ϱT_MS_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
 
 @register_symbolic ES.viscosity(model::ES.ChapmanEnskogModel, p, T, z::AbstractVector)
-@register_symbolic ES.viscosity(model::ES.ChapmanEnskogModel, T)
+@register_symbolic ES.viscosity(model::ES.ChapmanEnskogModel, T) false
 @register_symbolic ES.thermal_conductivity(model::ES.ChapmanEnskogModel, p, T, z::AbstractVector)
-@register_symbolic ES.thermal_conductivity(model::ES.ChapmanEnskogModel, T)
+@register_symbolic ES.thermal_conductivity(model::ES.ChapmanEnskogModel, T) false
 @register_symbolic ES.self_diffusion_coefficient(model::ES.ChapmanEnskogModel, p, T, z::AbstractVector)
-@register_symbolic ES.self_diffusion_coefficient(model::ES.ChapmanEnskogModel, T)
+@register_symbolic ES.self_diffusion_coefficient(model::ES.ChapmanEnskogModel, T) false
 @register_symbolic ES.MS_diffusion_coefficient(model::ES.ChapmanEnskogModel, p, T, z::AbstractVector)
 
 end
