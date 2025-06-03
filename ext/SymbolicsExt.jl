55c7f02e49ed97f5dbd056fda474151147305330
se-schmitt <se.s.schmitt@gmail.com>
2025-06-03 23:42:19 +0200

    Bug fixes

--- ext/SymbolicsExt.jl
+++ ext/SymbolicsExt.jl
@@ -8,6 +8,7 @@ const ES = EntropyScaling
 @register_symbolic ES.ϱT_thermal_conductivity(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
 @register_symbolic ES.ϱT_thermal_conductivity(model::ES.RefpropRESModel, ϱ, T, z::AbstractVector)
 @register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
+@register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T)
 @register_symbolic ES.ϱT_self_diffusion_coefficient(model::ES.FrameworkModel, ϱ, T, z::AbstractVector)
 @register_symbolic ES.ϱT_MS_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, ϱ, T, z::AbstractVector)
 
