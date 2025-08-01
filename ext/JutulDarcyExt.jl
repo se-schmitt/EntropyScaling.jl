01dfb9dab5229117aa1d7c46ca454b1ebe54b8c6
se-schmitt <se.s.schmitt@gmail.com>
2025-08-01 18:46:53 +0200

    update extension current error: ``` Cannot determine ordering of Dual tags ForwardDiff.Tag{Clapeyron.var"#fun#210"{MultiFluid{...}, ForwardDiff.Dual{Cells(), Float64, 2}, Vector{ForwardDiff.Dual{Cells(), Float64, 2}}}, Float64} and Cells() ```

--- ext/JutulDarcyExt.jl
+++ ext/JutulDarcyExt.jl
@@ -11,13 +11,21 @@ end
 ES.EStoJD(model::ES.AbstractEntropyScalingModel) = JDEntropyScalingModel(model)
 
 @jutul_secondary function update_viscosity!(mu, es::JDEntropyScalingModel{M}, 
-    model::JD.SimulationModel, p, T, flash_results, ix) where {M<:ES.AbstractEntropyScalingModel}
+    model::JD.SimulationModel, Pressure, Temperature, PhaseMassDensities, FlashResults, ix) where {M<:ES.AbstractEntropyScalingModel}
 
     sys = model.system
-    l, v = phase_indices(sys)
+    l, v = JD.phase_indices(sys)
+
     @inbounds for i in ix
-        mu[l,i] = viscosity(es.model, p, T, flash_results[i])
-        mu[v,i] = viscosity(es.model, p, T, flash_results[i])
+        # mu[l,i] = viscosity(es.model, Pressure[i], Temperature[i], FlashResults[i].liquid.mole_fractions; phase=:liquid)
+        # mu[v,i] = viscosity(es.model, Pressure[i], Temperature[i], FlashResults[i].vapor.mole_fractions; phase=:vapor)
+
+        Mwl = es.model.params[1].base.Mw' * FlashResults[i].liquid.mole_fractions
+        Mwv = es.model.params[1].base.Mw' * FlashResults[i].vapor.mole_fractions
+        ϱl = PhaseMassDensities[l,i] / Mwl
+        ϱv = PhaseMassDensities[v,i] / Mwv
+        mu[l,i] = ES.ϱT_viscosity(es.model, ϱl, Temperature[i], FlashResults[i].liquid.mole_fractions)
+        mu[v,i] = ES.ϱT_viscosity(es.model, ϱv, Temperature[i], FlashResults[i].vapor.mole_fractions)
     end
     
     return nothing
