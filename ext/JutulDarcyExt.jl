7c17621ffb18b8eb2b29132159cab2bdde7d032f
se-schmitt <se.s.schmitt@gmail.com>
2025-08-26 17:13:24 +0200

    pass only values (not full dual)

--- ext/JutulDarcyExt.jl
+++ ext/JutulDarcyExt.jl
@@ -4,6 +4,7 @@ using JutulDarcy, Jutul, EntropyScaling
 
 const ES = EntropyScaling
 const JD = JutulDarcy
+const FD = Jutul.ForwardDiff
 
 struct JDEntropyScalingModel{M} <: JD.PhaseVariables
     model::M
@@ -17,15 +18,13 @@ ES.EStoJD(model::ES.AbstractEntropyScalingModel) = JDEntropyScalingModel(model)
     l, v = JD.phase_indices(sys)
 
     @inbounds for i in ix
-        # mu[l,i] = viscosity(es.model, Pressure[i], Temperature[i], FlashResults[i].liquid.mole_fractions; phase=:liquid)
-        # mu[v,i] = viscosity(es.model, Pressure[i], Temperature[i], FlashResults[i].vapor.mole_fractions; phase=:vapor)
-
         Mwl = es.model.params[1].base.Mw' * FlashResults[i].liquid.mole_fractions
         Mwv = es.model.params[1].base.Mw' * FlashResults[i].vapor.mole_fractions
         ϱl = PhaseMassDensities[l,i] / Mwl
         ϱv = PhaseMassDensities[v,i] / Mwv
-        mu[l,i] = ES.ϱT_viscosity(es.model, ϱl, Temperature[i], FlashResults[i].liquid.mole_fractions)
-        mu[v,i] = ES.ϱT_viscosity(es.model, ϱv, Temperature[i], FlashResults[i].vapor.mole_fractions)
+
+        mu[l,i] = ES.ϱT_viscosity(es.model, FD.value(ϱl), FD.value(Temperature[i]), FD.value.(FlashResults[i].liquid.mole_fractions))
+        mu[v,i] = ES.ϱT_viscosity(es.model, FD.value(ϱv), FD.value(Temperature[i]), FD.value.(FlashResults[i].vapor.mole_fractions))
     end
     
     return nothing
