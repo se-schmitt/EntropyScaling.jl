3ee655b86ebd423d247f320f16e53f22c7314337
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-05-14 08:56:06 +0200

    [feat] initial split_model

---
+++ src/utils/split_models.jl
@@ -0,0 +1,37 @@
+# Singleton/metadata types are passed through unchanged
+CL.is_splittable(::AbstractTransportProperty) = false
+CL.is_splittable(::AbstractCollisionIntegralMethod) = false
+
+const SPLIT_TYPES = Union{AbstractEntropyScalingModel,AbstractParam,ChapmanEnskogModel,ParamVector}
+
+function CL._each_split_model(param::T, field, fieldname, group, Ic, Ig) where {T<:SPLIT_TYPES}
+    if !CL.is_splittable(field) || fieldname == :sources
+        return field
+    elseif isnothing(group) || field isa CL.EoSModel
+        return CL.each_split_model(field, Ic)
+    else
+        return CL.each_split_model(field, group, Ic, Ig)
+    end
+end
+
+CL.default_splitter(model::AbstractEntropyScalingModel) = 1:length(model)
+
+function CL.each_split_model(model::AbstractEntropyScalingModel, I)
+    if I isa AbstractVector{Bool}
+        return CL.each_split_model(model, findall(I))
+    end
+    if hasfield(typeof(model), :groups)
+        groups = model.groups
+        Ig = CL.create_group_splitter(groups, I)
+        return CL.each_split_model_struct(model, groups, I, Ig)
+    end
+    return CL.each_split_model_struct(model, I)
+end
+
+CL.each_split_model(model::ChapmanEnskogModel, I) = CL.each_split_model_struct(model, I)
+CL.each_split_model(params::ParamVector, I) = CL.each_split_model_struct(params, I)
+CL.each_split_model(param::AbstractParam, I) = CL.each_split_model_struct(param, I)
+
+# Vector of property-specific params: split each entry (component-wise)
+CL.each_split_model(v::AbstractVector{<:AbstractParam}, I) =
+    map(p -> CL.each_split_model(p, I), v)
