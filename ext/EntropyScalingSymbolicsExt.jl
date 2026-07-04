8e4dda5053524e0850c9f98dd0f2d3081e408b93
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-07-04 12:16:08 +0200

    [fix] rm warning from SmybolicsExt

--- ext/EntropyScalingSymbolicsExt.jl
+++ ext/EntropyScalingSymbolicsExt.jl
@@ -1,16 +1,21 @@
 module EntropyScalingSymbolicsExt
 
 using EntropyScaling, Symbolics
+using Symbolics.SymbolicUtils: BasicSymbolic
 
 const ES = EntropyScaling
 
 @register_symbolic ES.VT_viscosity(model::ES.AbstractEntropyScalingModel, V, T, z::AbstractVector)
 @register_symbolic ES.VT_thermal_conductivity(model::ES.AbstractEntropyScalingModel, V, T, z::AbstractVector)
-@register_symbolic ES.VT_thermal_conductivity(model::ES.RefpropRES, V, T, z::AbstractVector) false
 @register_symbolic ES.VT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, V, T, z::AbstractVector)
 @register_symbolic ES.VT_self_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, V, T) false
-@register_symbolic ES.VT_self_diffusion_coefficient(model::ES.ESFramework, V, T, z::AbstractVector) false
 @register_symbolic ES.VT_MS_diffusion_coefficient(model::ES.AbstractEntropyScalingModel, V, T, z::AbstractVector)
+
+for TV in (Num, BasicSymbolic, Real), TT in (Num, BasicSymbolic, Real)
+    TV === Real && TT === Real && continue
+    @eval ES.VT_thermal_conductivity(model::ES.RefpropRES, V::$TV, T::$TT, z::AbstractVector) =
+        Symbolics.wrap(Symbolics.term(ES.VT_thermal_conductivity, model, V, T, z))
+end
 
 @register_symbolic ES.viscosity(model::ES.ChapmanEnskogModel, p, T, z::AbstractVector)
 @register_symbolic ES.viscosity(model::ES.ChapmanEnskogModel, T) false
